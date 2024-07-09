21458e684f33b9cffe0f4f378d2e1da4c6b70271
Marcos Lora <marcoslora95@gmail.com>
2024-07-08 21:03:56 -0400

    Avances en la web

---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,16 @@
+<template>
+  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="text-3xl">Espere por favor</h1>
+    <h3 class="animete-pulse">Cargando pokemon</h3>
+  </section>
+  <section class="flex flex-col justify-center items-center items-center w-screen h-screen">
+    <h1>Quien es este Pokemon?</h1>
+    <PokemonPicture />
+    <PokemonOptions />
+  </section>
+</template>
+
+<script setup lang="ts">
+import PokemonPicture from '@/modules/pokemon/components/PokemonPicture.vue';
+import PokemonOptions from '@/modules/pokemon/components/PokemonOptions.vue';
+</script>
